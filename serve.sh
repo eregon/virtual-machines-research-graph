83ee9a923d210ce28c88af20724f49ef33190c3f
Stefan Marr <git@stefan-marr.de>
2020-03-15 14:10:13 +0000

    Replace .dot with .bib file
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ serve.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+PORT=7997
+HOST=127.0.0.1
+
+echo Starting Server
+echo "   Access: http://${HOST}:${PORT}/bibtex.html"
+
+if [ -x "$(command -v python3)" ]; then
+  python3 -m http.server ${PORT} --bind ${HOST}
+elif [ -x "$(command -v python2.7)" ]; then
+  python2.7 -m SimpleHTTPServer ${PORT} --bind ${HOST}
+elif [ -x "$(command -v php)" ]; then
+  php -S ${HOST}:${PORT}
+elif [ -x "$(command -v ruby)" ]; then
+  ruby -rwebrick -e"WEBrick::HTTPServer.new(:Port => ${PORT}, :DocumentRoot => Dir.pwd).start"
+fi
